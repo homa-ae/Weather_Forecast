703cf3b9fa6993dd8be681bbf6da0b1e6830f3d2
homa-ae <147973198+homa-ae@users.noreply.github.com>
2024-02-22 00:14:30 +0100

    Add files via upload

---
+++ build.py
@@ -0,0 +1,13 @@
+from pybuilder.core import use_plugin, init
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+
+name = "Weather Forecast"
+default_task = "publish"
+
+@init
+def initialize(project):
+    project.set_property("dir_source_main_python", "src")  
+    project.set_property("dir_source_unittest_python", "src/tests")  
+    project.depends_on("requests")  # Define dependencies
