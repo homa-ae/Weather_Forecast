e1d1f81e32378db0afd62912b2d33cfe145d5986
homa-ae <147973198+homa-ae@users.noreply.github.com>
2024-02-25 01:38:41 +0100

    Update build.py

--- build.py
+++ build.py
@@ -2,12 +2,13 @@ from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
 use_plugin("python.unittest")
+use_plugin("python.coverage")
 
-name = "Weather-Forecast"
+name = "Weather Forecast"
 default_task = "publish"
 
-#@init
-#def initialize(project):
-    #project.set_property("dir_source_main_python", "src")  
-    #project.set_property("dir_source_unittest_python", "src/tests")  
-    #project.depends_on("requests")  # Define dependencies
+@init
+def initialize(project):
+    project.set_property("dir_source_main_python", "src")  
+    project.set_property("dir_source_unittest_python", "src/tests")  
+    project.depends_on("requests")  
