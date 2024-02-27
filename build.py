4f1b501a27aed20c8c3cd1032685ecf86efc1388
homa-ae <147973198+homa-ae@users.noreply.github.com>
2024-02-27 20:30:09 +0100

    Update build.py

--- build.py
+++ build.py
@@ -3,6 +3,7 @@ from pybuilder.core import use_plugin, init
 use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.coverage")
+use_plugin("python.distutils")
 
 name = "Weather_Forecast"
 default_task = "publish"
